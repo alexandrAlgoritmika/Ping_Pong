c2b142915661e30fa6aa5f633fa7e95f9e01b716
alexandrAlgoritmika <algoritmikagame@gmail.com>
2025-04-13 19:30:37 +0500

    v1.6

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import pygame as pg
 pg.init() 
 
 class GameSprite(pg.sprite.Sprite):
-    def __init__(self, image, w, h, x, y):
+    def __init__(self, image, w, h, x, y, speed):
         super().__init__()
         self.image = pg.transform.scale(pg.image.load(image), (w,h))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.speed = speed
     def draw(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player1(GameSprite):
-    def move_up(self):
-        if self.rect.y > 0:
-            self.rect.y -= 5
-    def move_down(self):
-        if self.rect.y < 375:
-            self.rect.y += 5
+    def move(self):
+        self.rect.y -= self.speed
+
+class Player2(GameSprite):
+    def move(self):
+        self.rect.y -= self.speed
 
 win = pg.display.set_mode((850,500))
 pg.display.set_caption('PingPong')
@@ -26,9 +27,9 @@ win.fill((30,89,69))
 
 run = True
 clock = pg.time.Clock()
-ball = GameSprite('ball.png', 50, 50, 400, 225)
-player_1 = Player1('player_1.png', 125, 125, 10, 175)
-player_2 = GameSprite('player_2.png', 125, 125, 715, 175)
+ball = GameSprite('ball.png', 50, 50, 400, 225, 5)
+player_1 = Player1('player_1.png', 125, 125, 10, 175, 0)
+player_2 = Player2('player_2.png', 125, 125, 715, 175, 0)
 
 while run:
     for e in pg.event.get():
@@ -36,13 +37,28 @@ while run:
             run = False
         if e.type == pg.KEYDOWN:
             if e.key == pg.K_w:
-                player_1.move_up()
+                player_1.speed = 5
             if e.key == pg.K_s:
-                player_1.move_down()
+                player_1.speed = -5
+            if e.key == pg.K_UP:
+                player_2.speed = 5
+            if e.key == pg.K_DOWN:
+                player_2.speed = -5
+        if e.type == pg.KEYUP:
+            if e.key == pg.K_w:
+                player_1.speed = 0
+            if e.key == pg.K_s:
+                player_1.speed = 0
+            if e.key == pg.K_UP:
+                player_2.speed = 0
+            if e.key == pg.K_DOWN:
+                player_2.speed = 0
 
     win.fill((30,89,69))
     ball.draw()
+    player_1.move()
     player_1.draw()
+    player_2.move()
     player_2.draw()
     pg.display.update()
-    clock.tick(40)
+    clock.tick(60)
