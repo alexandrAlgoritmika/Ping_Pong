72f2dfc2a4ce5a60b6b68e8f1f70d9c56f18e0c2
alexandrAlgoritmika <algoritmikagame@gmail.com>
2025-04-13 19:12:50 +0500

    v1.5

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ class GameSprite(pg.sprite.Sprite):
     def draw(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player1(GameSprite):
+    def move_up(self):
+        if self.rect.y > 0:
+            self.rect.y -= 5
+    def move_down(self):
+        if self.rect.y < 375:
+            self.rect.y += 5
+
 win = pg.display.set_mode((850,500))
 pg.display.set_caption('PingPong')
 
@@ -18,11 +26,23 @@ win.fill((30,89,69))
 
 run = True
 clock = pg.time.Clock()
+ball = GameSprite('ball.png', 50, 50, 400, 225)
+player_1 = Player1('player_1.png', 125, 125, 10, 175)
+player_2 = GameSprite('player_2.png', 125, 125, 715, 175)
 
 while run:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             run = False
+        if e.type == pg.KEYDOWN:
+            if e.key == pg.K_w:
+                player_1.move_up()
+            if e.key == pg.K_s:
+                player_1.move_down()
+
     win.fill((30,89,69))
+    ball.draw()
+    player_1.draw()
+    player_2.draw()
     pg.display.update()
     clock.tick(40)
