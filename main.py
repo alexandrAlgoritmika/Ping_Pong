b1fa07fdd40d033d45c713dcd9945f72b3215049
alexandrAlgoritmika <algoritmikagame@gmail.com>
2025-04-13 18:39:04 +0500

    v1.3

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pygame as pg 
 pg.init() 
 
+class GameSprite(pg.sprite.Sprite):
+    def __init__(self, image, w, h, x, y):
+        super().__init__()
+        self.image = pg.transform.scale(pg.image.load(image), (w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
 win = pg.display.set_mode((850,500))
 pg.display.set_caption('PingPong')
 
