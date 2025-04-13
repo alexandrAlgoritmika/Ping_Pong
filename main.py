116da0020ffec485df6ca7cd8feb7d7615c29759
alexandrAlgoritmika <algoritmikagame@gmail.com>
2025-04-13 18:32:54 +0500

    v1.2

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hi')
+import pygame as pg 
+pg.init() 
+
+win = pg.display.set_mode((850,500))
+pg.display.set_caption('PingPong')
+
+win.fill((30,89,69))
+
+run = True
+clock = pg.time.Clock()
+
+while run:
+    for e in pg.event.get():
+        if e.type == pg.QUIT:
+            run = False
+    win.fill((30,89,69))
+    pg.display.update()
+    clock.tick(40)
